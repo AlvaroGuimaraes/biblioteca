a34ec704c6969de44b787d3272b75bf8e8cd35ef
Alvaro Neto <alvarojguimaraes@hotmail.comm>
2021-09-25 01:17:39 -0300

    testando commit

--- src/controller/usuario.controller.ts
+++ src/controller/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsuarioService } from '../services/usuario.service';
 import { Usuario } from 'src/models/usuario.model';
 
